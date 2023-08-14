2357efae39bd512252e4679d0c12f5c5406dc6d6
krystiano13 <crystianotv@gmail.com>
2023-08-14 19:40:14 +0200

    importing fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,14 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const autoprefixer = require("gulp-autoprefixer");
 const minifyCss = require("gulp-clean-css");
-const concat = require("gulp-concat");
 
 const compileSass = () => {
-  return src("./styles/sass/**/*.scss")
+  return src("./styles/sass/*.scss")
     .pipe(sass())
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ["last 2 versions"],
-      })
-    )
     .pipe(minifyCss())
-    .pipe(concat("style.css"))
     .pipe(dest("./styles/css/"));
 };
 
 const watchTask = () => watch(["./styles/sass/**/*.scss"], compileSass);
 
-exports.default = series(watchTask, compileSass);
+exports.default = series(watchTask,compileSass);
