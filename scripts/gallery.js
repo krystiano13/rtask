dd2077800ed2e14aafa61413525af9e32496dc8f
krystiano13 <crystianotv@gmail.com>
2023-08-20 18:00:33 +0200

    image gallery popup functionality

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,4 +1,10 @@
 const images = document.querySelector('.Gallery__images');
+const popUpGalleryImage = document.querySelector('.GalleryPopup__images__image');
+const popUpButtonLeft = document.querySelector('#popUpLeft');
+const popUpButtonRight = document.querySelector("#popUpRight");
+
+let popUpImageId = 1;
+
 const macy = Macy({
     container: images,
     margin: 50,
@@ -9,3 +15,17 @@ const macy = Macy({
         }
     }
 });
+
+const changeImage = (left) => {
+    if (left && popUpImageId > 1) {
+        popUpImageId--;
+    }
+    else if (!left && popUpImageId < 12) {
+      popUpImageId++;
+    }
+
+    popUpGalleryImage.src = `./assets/galleryImages/gallery${popUpImageId}.webp`;
+}
+
+popUpButtonLeft.addEventListener('click', () => changeImage(true));
+popUpButtonRight.addEventListener('click', () => changeImage(false));
