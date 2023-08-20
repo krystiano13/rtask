435d6d2a2bd514bd5559ad429b58aa844cbf5603
krystiano13 <crystianotv@gmail.com>
2023-08-20 12:09:26 +0200

    add masonry layout for image gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,1 +1,11 @@
 const images = document.querySelector('.Gallery__images');
+const macy = Macy({
+    container: images,
+    margin: 50,
+    columns: 3,
+    breakAt: {
+        992: {
+            columns : 1
+        }
+    }
+});
